818665b6773172cd8fdc7406083d992372d713c4
Taro Omiya <taro.omiya@omiyagames.com>
2020-02-27 23:23:17 -0500

    Create resume-block.php
    Making a basic plugin file.

---
+++ resume-block.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Resume Block
+ * Plugin URI: https://github.com/japtar10101/resume-block
+ * Description: Allow creating a timeline-like visuals with Gutenberg blocks, making it useful for creating a resume page.
+ * Version: 1.0.0
+ * Author: Taro Omiya
+ * Author URI: https://taroomiya.com
+ * License: GPL v2
+ **/
+?>
