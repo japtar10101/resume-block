347db4d0b6592e3bbfb6330214ead37c2e8a107f
Taro Omiya <taro.omiya@omiyagames.com>
2020-03-02 20:13:29 -0500

    Making the javascript only load on block editor.

--- resume-block.php
+++ resume-block.php
@@ -9,19 +9,29 @@
  * License: GPL v2
  **/
 
-/**
- * Enqueue css styles.
- */
-function resume_block_styles_and_scripts() {
-	// Stylesheets go here.
-	wp_enqueue_style( 'font-awesome', '//use.fontawesome.com/releases/v5.12.1/css/all.css', false, '5.12.1', 'all' );
-	wp_enqueue_style( 'bulma', plugins_url( 'bulma/bulma.min.css', __FILE__ ), false, '0.8.0', 'all' );
-	wp_enqueue_style( 'bulma-timeline', plugins_url( 'bulma/timeline/bulma-timeline.min.css', __FILE__ ), false, '2.3.2', 'all' );
+if ( ! function_exists( 'resume_block_styles' ) ) :
+	/**
+	 * Enqueue css styles.
+	 */
+	function resume_block_styles() {
 
-	// Javascripts go here
-	wp_enqueue_script( 'bulma-timeline', plugins_url( 'resume-block.js', __FILE__ ) );
-}
-add_action( 'wp_enqueue_scripts', 'resume_block_styles_and_scripts' );
+		// Stylesheets go here.
+		wp_enqueue_style( 'font-awesome', '//use.fontawesome.com/releases/v5.12.1/css/all.css', false, '5.12.1', 'all' );
+		wp_enqueue_style( 'bulma', plugins_url( 'bulma/bulma.min.css', __FILE__ ), false, '0.8.0', 'all' );
+		wp_enqueue_style( 'bulma-timeline', plugins_url( 'bulma/timeline/bulma-timeline.min.css', __FILE__ ), false, '2.3.2', 'all' );
+	}
+endif;
+add_action( 'wp_enqueue_scripts', 'resume_block_styles' );
+
+if ( ! function_exists( 'resume_block_scripts' ) ) :
+	/**
+	 * Enqueue javascripts.
+	 */
+	function resume_block_scripts() {
+		wp_enqueue_script( 'resume-block', plugins_url( 'resume-block.js', __FILE__ ), array( 'wp-blocks' ) );
+	}
+endif;
+add_action( 'enqueue_block_editor_assets', 'resume_block_scripts' );
 
 /**
  * FIXME: do something!
