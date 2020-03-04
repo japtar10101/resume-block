5fee1af7ee313542a4d5bac66f3c3738211a2738
Taro Omiya <taro.omiya@omiyagames.com>
2020-03-03 22:31:21 -0500

    Copied the minified css to a non-minified bulma-timeline.css with all the classes replaced with the wp-block equivalent.

--- resume-block.php
+++ resume-block.php
@@ -28,7 +28,7 @@ if ( ! function_exists( 'resume_block_enqueue_style' ) ) :
 		// Stylesheets go here.
 		wp_enqueue_style( 'font-awesome', '//use.fontawesome.com/releases/v5.12.1/css/all.css', false, '5.12.1', 'all' );
 		wp_enqueue_style( 'bulma', plugins_url( 'bulma/bulma.min.css', __FILE__ ), false, '0.8.0', 'all' );
-		wp_enqueue_style( 'bulma-timeline', plugins_url( 'bulma/timeline/bulma-timeline.min.css', __FILE__ ), false, '2.3.2', 'all' );
+		wp_enqueue_style( 'bulma-timeline', plugins_url( 'bulma/timeline/bulma-timeline.css', __FILE__ ), false, '2.3.2', 'all' );
 	}
 endif;
 add_action( 'enqueue_block_assets', 'resume_block_enqueue_style' );
